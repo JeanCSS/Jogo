908fa92b2234b10beaf4db4d8d1038941fc12476
Jean Cordeiro da Silva <jeancordeirodasilva@MacBook-de-Jean.local>
2020-04-12 21:43:42 -0300

    add o socket-io

--- playground/server.js
+++ playground/server.js
@@ -1,11 +1,26 @@
 import express from "express"
 import http from "http"
+import createGame from "./public/game.js"
+import socketio from "socket.io"
 
 const app = express()
 const server = http.createServer(app)
+const sockets = socketio(server)
 
 app.use(express.static('public'))
 
-server.listen(8080, () => {
-  console.log('> Server listing on port: 8080')
+const game = createGame()
+game.addPlayer({playerId: "player1", playerX: 1, playerY: 1})
+game.addFruit({fruitId: "fruit1", fruitX: 3, fruitY: 5})
+game.addFruit({fruitId: "fruit2", fruitX: 4, fruitY: 2})
+console.log(game.state)
+
+sockets.on('connection', (socket) => {
+  const playerId = socket.id
+  socket.emit('setup', game.state)
+  console.log(`> Player connected on server whith id: ${playerId}`)
+})
+
+server.listen(3000, () => {
+  console.log('> Server listing on port: 3000')
 }) 
